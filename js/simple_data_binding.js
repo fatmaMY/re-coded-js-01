069fbccdf130b742a385d263062cff44d243f9e7
fatmaMY <fatima.mohammad736@gmail.com>
2017-02-28 22:11:48 +0300

    my first try to solve the task

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  
+  dataBinding();
 });
+
+function dataBinding() {
+	document.getElementById("textArea").addEventListener("keypress", bindValues());
+}
+
+function bindValues() {
+ 
+  //$('#myDiv').append($('textarea').val());
+document.getElementById("myDiv").append($('textarea').val());
+
+}
+
+
+
+
